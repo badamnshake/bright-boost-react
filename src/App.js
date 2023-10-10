083308364fb9a40a7558399e7156caae1242485e
Kartavya Gohil <83202563+badamnshake@users.noreply.github.com>
2023-10-10 18:42:37 +1100

    add calendar faq, and form page and add new packag

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ import Sidebar from "./scenes/global/Sidebar";
 import AdminDashboard from "./scenes/admin/admin-dashboard";
 
 import Team from "./scenes/admin/team";
-// import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
-// import Calendar from "./scenes/calendar";
 import Contacts from './scenes/admin/contacts';
 import Invoices from './scenes/admin/invoices';
+import Form from './scenes/admin/form';
+import Calendar from './scenes/student/calendar';
+import FAQ from './scenes/student/faq';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -36,13 +30,13 @@ function App() {
               <Route path="/team" element={<Team />}></Route>
               <Route path="/contacts" element={<Contacts />}></Route>
               <Route path="/invoices" element={<Invoices />}></Route>
-              {/* <Route path="/form" element={<Form />}></Route> */}
+              <Route path="/form" element={<Form />}></Route>
               {/* <Route path="/bar" element={<Bar />}></Route> */}
               {/* <Route path="/pie" element={<Pie />}></Route> */}
               {/* <Route path="/line" element={<Line />}></Route> */}
-              {/* <Route path="/faq" element={<FAQ />}></Route> */}
+              <Route path="/faq" element={<FAQ />}></Route>
               {/* <Route path="/geography" element={<Geography />}></Route> */}
-              {/* <Route path="/calendar" element={<Calendar />}></Route> */}
+              <Route path="/calendar" element={<Calendar />}></Route>
 
             </Routes>
           </main>
